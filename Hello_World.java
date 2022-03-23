58f198389a1779c07e8bb8f5e6fd0f5b56a57647
RISHAVIITKGP <37202812+RISHAVIITKGP@users.noreply.github.com>
2022-03-23 17:48:44 +0900

    Update wrong changes

--- Hello_World.java
+++ Hello_World.java
@@ -1,7 +1,7 @@
 # first java 
 # another comment added
 public class Hello {
- public public static void main(String[] args) {
+ public public public static void main(String[] args) {
 	System.out.println("Hello, World");
 	}
   }	
